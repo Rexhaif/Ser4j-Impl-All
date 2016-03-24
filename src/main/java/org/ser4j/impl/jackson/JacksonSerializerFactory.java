eb5af929006cc4c561b2690d1f7edc64b7b3f96b
Rexhaif <larionov.messerschmitt@gmail.com>
2016-03-25 00:24:22 +0300

    initial commit

--- src/main/java/org/ser4j/impl/jackson/JacksonSerializerFactory.java
+++ src/main/java/org/ser4j/impl/jackson/JacksonSerializerFactory.java
@@ -1,7 +1,47 @@
 package org.ser4j.impl.jackson;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ser4j.core.SerializedForm;
+import org.ser4j.core.Serializer;
+import org.ser4j.core.impl.serializedform.StringSerializedForm;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
 /**
- * Created by Даниил on 24.03.2016.
+ * Factory of jackson's serializers for various data formats
  */
 public class JacksonSerializerFactory {
+
+    public static Serializer createSerializer(JsonFactory formatFactory, Consumer<Throwable> exceptionCallback) {
+        return new Serializer() {
+
+            private ObjectMapper mapper = new ObjectMapper(formatFactory);
+
+            @Override
+            public <T> T deserialize(SerializedForm serializedForm, Class<T> aClass) {
+                T object = null;
+                try {
+                    object =  mapper.readValue(serializedForm.asByteArray(), aClass);
+                } catch (IOException e) {
+                    exceptionCallback.accept(e);
+                }
+                return object;
+            }
+
+            @Override
+            public <T> SerializedForm serialize(T t) {
+                SerializedForm serializedForm = null;
+                try {
+                    serializedForm = new StringSerializedForm(mapper.writeValueAsString(t));
+                } catch (JsonProcessingException e) {
+                    exceptionCallback.accept(e);
+                }
+                return serializedForm;
+            }
+        };
+    }
+
 }
--- src/main/java/org/ser4j/impl/jackson/JacksonSerializers.java
+++ src/main/java/org/ser4j/impl/jackson/JacksonSerializers.java
@@ -1,7 +1,54 @@
 package org.ser4j.impl.jackson;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
+import com.fasterxml.jackson.dataformat.smile.SmileFactory;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.ser4j.core.Serializer;
+import org.ser4j.core.Util;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Created by Даниил on 24.03.2016.
+ * Exporting end point
  */
 public class JacksonSerializers {
+
+    private static Map<String, Serializer> jacksonSerializers = new HashMap<>();
+
+    static {
+        addComplexSerializers("json",  new JsonFactory());
+        addComplexSerializers("yaml",  new YAMLFactory());
+        addComplexSerializers("cbor",  new CBORFactory());
+        addComplexSerializers("smile", new SmileFactory());
+        addComplexSerializers("xml",   new XmlFactory());
+    }
+
+    private static void addSerializer(String name, Serializer serializer) {
+        jacksonSerializers.put(name, serializer);
+    }
+
+    private static void addComplexSerializers(String formatName, JsonFactory factory) {
+        addSerializer(
+                "jackson/" + formatName,
+                JacksonSerializerFactory.createSerializer(
+                        factory,
+                        Throwable::printStackTrace
+                )
+        );
+        addSerializer(
+                "jackson/" + formatName + "/ignore_error",
+                JacksonSerializerFactory.createSerializer(
+                        factory,
+                        Util::doNothingWithThrowable
+                )
+        );
+    }
+
+    public static Map<String, Serializer> export() {
+        return jacksonSerializers;
+    }
+
 }
